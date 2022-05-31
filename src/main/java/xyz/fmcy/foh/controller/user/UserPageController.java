700ca1f439a0d7db94fa8ff28ccaf45710438a86
付高宏 <2468058616@qq.com>
2022-05-31 17:23:01 +0800

    foh_start

--- src/main/java/xyz/fmcy/foh/controller/user/UserPageController.java
+++ src/main/java/xyz/fmcy/foh/controller/user/UserPageController.java
@@ -1,9 +1,17 @@
 package xyz.fmcy.foh.controller.user;
 
+import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.expression.Messages;
+import xyz.fmcy.foh.pojo.User;
 import xyz.fmcy.foh.service.UserService;
 
 import javax.annotation.Resource;
@@ -14,18 +22,15 @@ public class UserPageController {
     private UserService userService;
 
     @RequestMapping("/login")
-    String loginPage(Model model){
-        model.addAttribute("loginErr"  ,"noterr");
+    String loginPage(@ModelAttribute("user") User user) {
         return "/login";
     }
 
-    @GetMapping("/login/has/user")
-    String hasUserByName(Model model,String username){
-        if (userService.hasUserByUserName(username)) {
-            model.addAttribute("loginErr"  ,"noterr");
-        }else {
-            model.addAttribute("loginErr","err1");
+    @PostMapping("/user/login")
+    String login(@ModelAttribute("user") @Validated User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/login";
         }
-        return "/login::loginError";
+        return "/index";
     }
 }
--- src/main/java/xyz/fmcy/foh/pojo/User.java
+++ src/main/java/xyz/fmcy/foh/pojo/User.java
@@ -1,9 +1,21 @@
 package xyz.fmcy.foh.pojo;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class User {
     private Integer uid;
+
+    @NotEmpty(message = "用户名不能为空")
+    @Size(min = 4,max = 20,message = "用户名字符位数在4-20位之间")
+    @Pattern(regexp = "^[a-zA-Z\\d]{4,20}$",message = "")
     private String username;
+
+    @NotEmpty(message = "密码不能为空")
+    @Size(min = 6,max = 30,message = "密码字符位数在6-30位之间")
     private String password;
+    @Size(max = 20)
     private String nickname;
 
     public User() {
